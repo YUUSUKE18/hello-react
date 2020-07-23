ee5ce43bb5e84452dc6148dc94b927d804f5e94b
Yuusuke Okamoto <yuusuke.o.work.1018@gmail.com>
2020-07-23 11:44:27 +0900

    Add Plugins

--- portfolio/gatsby-config.js
+++ portfolio/gatsby-config.js
@@ -1,10 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: `Hello Yuusuke`,
-    description: `This is Yuusuke's Portfolio Website!`,
-    author: `Yuusuke Okamoto`,
+    title: "Hello Yuusuke || Home",
+    description: "This is Yuusuke's Portfolio Website!",
+    siteUrl: "https://www.hello-yuusuke.work",
+    author: "Yuusuke Okamoto",
+    twitter: "yuusukesan_EN",
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Yuusuke Okamoto`,
+        short_name: `yuusuke`,
+        start_url: `/`,
+        background_color: `#337ab7`,
+        theme_color: `#337ab7`,
+        display: `fullscreen`,
+        icon: `src/static/logo/favicon.png`,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -12,23 +26,16 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+      {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+    `gatsby-plugin-sitemap`,
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
