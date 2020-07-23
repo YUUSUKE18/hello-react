3f7c355c8e165090f2975f5b3125a7d17a954f36
Yuusuke Okamoto <yuusuke.o.work.1018@gmail.com>
2020-07-23 11:33:37 +0900

    Change siteMetaData

--- portfolio/gatsby-config.js
+++ portfolio/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hello Yuusuke`,
+    description: `This is Yuusuke's Portfolio Website!`,
+    author: `Yuusuke Okamoto`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
